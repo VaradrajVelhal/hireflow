2fde92503e1e2ff1fa9bcfc337637043b9ea6423
Varadraj Velhal <varadrajvelhal1@gmail.com>
2026-05-01 22:39:03 +0530

    disable email for deployment

--- backend/hireflow/settings.py
+++ backend/hireflow/settings.py
@@ -139,8 +139,8 @@ SIMPLE_JWT = {
 }
 AUTH_USER_MODEL = 'accounts.User'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
