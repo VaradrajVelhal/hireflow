79c24fcc95b581fa779a69a6cc5a47adbd493edf
Varadraj Velhal <varadrajvelhal1@gmail.com>
2026-07-31 22:43:24 +0530

    Fixed some bugs

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://hireflow-1-qr4c.onrender.com/api/",
 });
 
 API.interceptors.request.use((req) => {
